7e700f2e3e120743b6e199ac69855bec8032f3c9
schackattack89 <nschack@gmail.com>
2024-05-22 15:42:54 -0400

    button responsiveness

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,6 @@
 const currentYear = new Date().getFullYear();
 const yearElement = document.querySelector('#year');
-yearElement.textContent = `\u00A9${currentYear}`;
+yearElement.textContent = `\u00A9${currentYear} - Edward Schack - Scottsburg, IN (USA)`;
 
 let oLastModif = new Date(document.lastModified);
 const lastModifiedElement = document.querySelector('#lastModified');
