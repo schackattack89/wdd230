514222731afd876d1bf0ccd68a305b085ce4aa30
schackattack89 <nschack@gmail.com>
2024-05-23 16:45:22 -0400

    darkmode

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -1,8 +1,15 @@
-const modeButton = document.querySelector("#mode");
-const body = document.querySelector("body");
-const main = document.querySelector("main");
+// const modeButton = document.querySelector("#mode");
+// const body = document.querySelector("body");
+// const main = document.querySelector("main");
 
-modeButton.addEventListener("click", () => {
-	body.classList.toggle('dark-mode');
-	main.classList.toggle('dark-mode');
+// modeButton.addEventListener("click", () => {
+// 	body.classList.toggle('dark-mode');
+// 	main.classList.toggle('dark-mode');
+// });
+
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', ()=>{
+    body.classList.toggle('dark-mode');
 });
