d3de2fd6614cd8a1e5724d1f751373f878922ccc
schackattack89 <nschack@gmail.com>
2024-05-23 12:21:34 -0400

    lesson05

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () =>{
+    if(input.value != ''){
+        const listItem = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        listItem.innerHTML = input.value;
+        deleteButton.textContent = '✖';
+        listItem.append(deleteButton);
+        list.append(listItem);
+        deleteButton.addEventListener('click', () =>{
+            list.removeChild(listItem);
+            input.focus();
+        });
+        input.value = '';
+    }
+    input.focus();
+});
