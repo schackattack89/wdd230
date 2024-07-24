907eb8c0b89113cc9a92b2241aecf7781664c048
schackattack89 <nschack@gmail.com>
2024-07-24 14:01:31 -0400

    img dump

--- scoots/scripts/quantitylimiter.js
+++ scoots/scripts/quantitylimiter.js
@@ -1,3 +1,5 @@
+// Borrowed function attribute syntax from W3 Schools
+
 document.getElementById('quantity').addEventListener('input', function() {
     // Remove non-numeric characters from the input value
     this.value = this.value.replace(/[^1-5]/g, '');
